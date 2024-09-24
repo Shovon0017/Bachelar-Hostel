321f13ba136c813ae6bf9b9774ec9716e5ab1cd4
Shovon0017 <Shimantomostofa@gmail.com>
2024-09-24 23:31:11 +0600

    bachelor hostel

--- lib/controller/api-controller/product_description.dart
+++ lib/controller/api-controller/product_description.dart
@@ -2,9 +2,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 
 import '../../database/hostelInfo.dart';
-import '../../model/productInfoModel.dart';
+import '../../model/hostelInfoModel.dart';
 class HostelInfoService {
-  static Future<ProductInfoModel?> hostelInfoService({required String id}) async {
+  static Future<vv?> hostelInfoService({required String id}) async {
     try {
       ProductInfoModel descriptionModel = ProductInfoModel.fromJson(jsonDecode(jsonEncode(ProductInfoData.productInfoData)));
       return descriptionModel;
---
+++ lib/model/hostelInfoModel.dart
@@ -0,0 +1,71 @@
+class HostelInfoModel {
+  List<Hostel>? hostel;
+
+  HostelInfoModel({this.hostel});
+
+  HostelInfoModel.fromJson(Map<String, dynamic> json) {
+    if (json['hostel'] != null) {
+      hostel = <Hostel>[];
+      json['hostel'].forEach((v) {
+        hostel!.add(new Hostel.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.hostel != null) {
+      data['hostel'] = this.hostel!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Hostel {
+  String? name;
+  String? id;
+  String? address;
+  String? number;
+  String? email;
+  int? price;
+  String? description;
+  String? environment;
+  String? image;
+
+  Hostel(
+      {this.name,
+        this.id,
+        this.address,
+        this.number,
+        this.email,
+        this.price,
+        this.description,
+        this.environment,
+        this.image});
+
+  Hostel.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    id = json['id'];
+    address = json['address'];
+    number = json['number'];
+    email = json['email'];
+    price = json['price'];
+    description = json['description'];
+    environment = json['Environment'];
+    image = json['image'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['id'] = this.id;
+    data['address'] = this.address;
+    data['number'] = this.number;
+    data['email'] = this.email;
+    data['price'] = this.price;
+    data['description'] = this.description;
+    data['Environment'] = this.environment;
+    data['image'] = this.image;
+    return data;
+  }
+}
